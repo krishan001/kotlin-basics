861f816875175196123307adad708be54510d2a6
Krishan Patel <sc17kdp@leeds.ac.uk>
2020-03-03 13:23:19 +0000

    check for number of arguments

--- cwk2/src/main/kotlin/comp3222/cwk2/main.kt
+++ cwk2/src/main/kotlin/comp3222/cwk2/main.kt
@@ -6,8 +6,10 @@ fun main(args: Array<String>){
     val rd = RainfallDataset()
     if (args.size == 1){
         rd.readFile(args[0])
-    }else{
+    }else if(args.size == 2){
         rd.printBarChart(args[0],args[1])
+    }else{
+        println("You have entered an invalid number of arguments")
     }
 
 
